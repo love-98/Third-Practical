efd34ba77cb3816b1e32586993087f64a6d59133
love-98 <131331709+love-98@users.noreply.github.com>
2023-04-27 11:35:44 +0530

    Update updatedescription.php

--- updatedescription.php
+++ updatedescription.php
@@ -26,7 +26,7 @@
         </div>
       </nav>
       <div class="container">
-        <form <?php $SNO=$_GET['SNO']; echo "action='updatedescription.php?SNO=$SNO'";?> method="post">
+        <form <?php $SNO=$_GET['SRNO']; echo "action='updatedescription.php?SNO=$SNO'";?> method="post">
             <label for="pagetitle" class="form-label"> Page Title</label>
             <input type="text" id="pagetitle" name="pagetitle" class="form-control">
             <label for="pageslug" class="form-label"> Page Slub</label>
